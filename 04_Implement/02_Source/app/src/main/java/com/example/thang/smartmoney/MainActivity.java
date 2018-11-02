e1302d6b22de668671c4a93699dd9935d4112457
beohoang98 <beohoang98@users.noreply.github.com>
2018-11-02 23:50:29 +0700

    [Src] add source cua Thang

---
+++ 04_Implement/02_Source/app/src/main/java/com/example/thang/smartmoney/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.thang.smartmoney;
+
+import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnLogin,btnSignup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        AnhXa();
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,Login_Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,Signup_Activity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    void AnhXa()
+    {
+        btnLogin  =findViewById(R.id.btnLogin);
+        btnSignup =findViewById(R.id.btnSignup);
+    }
+}
