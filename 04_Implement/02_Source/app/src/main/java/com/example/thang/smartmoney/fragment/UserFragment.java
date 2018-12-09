929eaf5eb006172bc82fd27872f777f642310896
beohoang98 <beohoang98@users.noreply.github.com>
2018-12-09 23:54:36 +0700

    test thu chuc nang sync len Firebase, working duoc 1 phan

--- 04_Implement/02_Source/app/src/main/java/com/example/thang/smartmoney/fragment/UserFragment.java
+++ 04_Implement/02_Source/app/src/main/java/com/example/thang/smartmoney/fragment/UserFragment.java
@@ -1,26 +1,36 @@
 package com.example.thang.smartmoney.fragment;
 
 import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.AppCompatImageView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.thang.smartmoney.MainActivity;
 import com.example.thang.smartmoney.R;
+import com.example.thang.smartmoney.database.FirebaseSync;
 import com.squareup.picasso.Picasso;
 
-public class UserFragment extends Fragment {
+import java.util.concurrent.Callable;
+
+public class UserFragment extends Fragment implements View.OnClickListener {
 
     View view;
     Button logoutButton;
+    Button syncButton;
+    ProgressBar progressBar;
+
     private Uri avatar_url;
     private String username;
     private String email;
@@ -28,6 +38,7 @@ public class UserFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FirebaseSync.Init(getContext());
     }
 
     public void AnhXa() {
@@ -45,6 +56,10 @@ public class UserFragment extends Fragment {
                 LogOut();
             }
         });
+
+        progressBar = view.findViewById(R.id.progressBar);
+        syncButton = view.findViewById(R.id.sync_btn);
+        syncButton.setOnClickListener(this);
     }
 
     @Nullable
@@ -67,6 +82,31 @@ public class UserFragment extends Fragment {
         MainActivity.mAuth.signOut();
         Intent login = new Intent(getActivity(), MainActivity.class);
         startActivity(login);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.sync_btn) {
+            Log.d("Sync", "SYnc button clicked");
+            progressBar.setVisibility(View.VISIBLE);
+            final ColorStateList oldColorTint = syncButton.getBackgroundTintList();
+            syncButton.setBackgroundTintList(getResources().getColorStateList(R.color.lightgrey));
+
+            FirebaseSync.afterSync("btnSyncClick", new Callable() {
+                @Override
+                public Object call() throws Exception {
+                    if (progressBar.getVisibility() != View.GONE) {
+                        syncButton.setBackgroundTintList(oldColorTint);
+                        progressBar.setVisibility(View.GONE);
+                    }
+
+                    Toast.makeText(getContext(), "Sync complete", Toast.LENGTH_SHORT).show();
+                    Log.d("Sync", "Sync completed");
+                    return null;
+                }
+            });
+            FirebaseSync.syncDatabase();
+        }
     }
 }
 
